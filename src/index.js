e301d0b7264b745e54fe0199414bd79cf4ecba02
levon <levon.gambaryan@gmail.com>
2019-07-23 23:08:49 +0300

    small fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Favicon from './images/favicon.jpg'; // eslint-disable-line no-unused-vars
 
 import './styles/_stylebase.scss';
-// import someimage from './images/layout/someimagе.png';
-// import somescript from './js/somescript';
 
 import Form from './js/form';
 
